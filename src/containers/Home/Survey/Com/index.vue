cca9f3e4d7c89881a728f5ecc426c9dff13c8399
王六锋 <2496436621@qq.com>
2020-04-01 13:34:47 +0800

    处理函数

--- src/containers/Home/Survey/Com/index.vue
+++ src/containers/Home/Survey/Com/index.vue
@@ -2,25 +2,40 @@
   <div>
     <div>{{ num }}</div>
     <div class="p-t-30">
-      <a-button type="dashed" @click="handleChange1">Dashed</a-button>
+      <a-button type="dashed" @click="handleChange1(9)">Dashed</a-button>
     </div>
     <div class="p-t-30">
-      <a-button type="danger" @click="handleChange2">Danger</a-button>
+      <a-button type="danger" @click="handleChange2(1)">Danger</a-button>
     </div>
   </div>
 </template>
 
 <script>
+import { mapMutations } from "vuex";
 export default {
   data: () => ({}),
   components: {},
-  mounted() {},
+  mounted() {
+    this.studyforEach();
+  },
   methods: {
-    handleChange1() {
-      this.$store.commit("add",9);
-    },
-    handleChange2() {
-      this.$store.commit("min",1);
+    ...mapMutations({
+      handleChange1: "add",
+      handleChange2: "min"
+    }),
+    studyforEach() {
+      const arr = [1, 2, 3];
+      const brr = [];
+      arr.map((a, b, c) => {
+        setTimeout(() => {
+          a * 10;
+          {
+            b, c;
+          }
+          brr.push(a * 10);
+        }, 0);
+      });
+      console.log(brr);
     }
   },
   watch: {},
