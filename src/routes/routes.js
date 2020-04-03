fb86ebcbdca84933ff65167f47257618d0d3ee87
王六锋 <2496436621@qq.com>
2020-04-03 09:41:33 +0800

    no message

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,6 +5,8 @@ import { routes } from "./index";
 Vue.use(VueRouter);
 // router
 const router = new VueRouter({
+  // 如果不设置会优先采用锚点路由，采用此方式，需要后台配nginx把所有页面指向index.html
+  // mode: "history",
   routes // (缩写) 相当于 routes: routes
 });
 // 路由守卫
--- vue.config.js
+++ vue.config.js
@@ -7,5 +7,18 @@ module.exports = {
         changeOrigin: true
       }
     }
+  },
+  // 此处可以修改antd主题颜色
+  css: {
+    loaderOptions: {
+      less: {
+        modifyVars: {
+          "primary-color": "#1DA57A",
+          "link-color": "#1DA57A",
+          "border-radius-base": "2px"
+        },
+        javascriptEnabled: true
+      }
+    }
   }
 };
